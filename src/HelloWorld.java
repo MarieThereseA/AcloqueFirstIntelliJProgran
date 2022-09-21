8c127ef0e2a5a88bf011ab6467bb82bf80ecc052
I\1E10 Laptop <sample@bths.edu>
2022-09-21 12:22:51 -0400

    added user input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+        System.out.print("Please enter your first number: ");
+        int num1 = scan.nextInt();
+        System.out.print("Please enter your second number: ");
+        int num2 = scan.nextInt();
+
+        double avg = (num1 + num2) / 2.0;
+
+        System.out.println("The average of " + num1 + " and " + num2 + " is " + avg + "!");
+    }
+
 }
